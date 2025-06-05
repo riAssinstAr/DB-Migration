226779f3a3a94044b9df5c7251027194a0b715b4
riAs-g <abhi2308204@gmail.com>
2025-06-06 01:46:39 +0530

    Fourth Commit

--- src/main/java/com/example/migration/MigrationApplication.java
+++ src/main/java/com/example/migration/MigrationApplication.java
@@ -3,10 +3,18 @@ package com.example.migration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class MigrationApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("SUPABASE_DB_URL", dotenv.get("SUPABASE_DB_URL"));
+		System.setProperty("SUPABASE_DB_USER", dotenv.get("SUPABASE_DB_USER"));
+		System.setProperty("SUPABASE_DB_PASS", dotenv.get("SUPABASE_DB_PASS"));
+
 		SpringApplication.run(MigrationApplication.class, args);
 	}
 
